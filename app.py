ad220921db82fc49abf5151b40828df06cbc0c32
p1v0tal <2531220201@qq.com>
2025-04-25 11:34:36 +0800

    4/25 init

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from config import Config,db
 
+from models import House
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
@@ -8,6 +10,8 @@ db.init_app(app)
 #测试代码
 @app.route('/')
 def hello():
+    first_house =House.query.first()
+    print(first_house)
     return render_template('index.html')
 
 if __name__ == '__main__':
